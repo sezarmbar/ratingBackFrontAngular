0c6f72d51489260efbf3ef59d7228083d3852f51
sezarmbar <sezar_mbar@yahoo.com>
2017-08-04 12:28:13 +0200

    add home Component

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import {NgModule, ApplicationRef} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpModule, JsonpModule} from '@angular/http';
-import {ServiceAppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, ApplicationRef, APP_INITIALIZER } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ServiceAppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+
+import {
+  HeaderComponent
+} from './component';
 
 import {
   MdAutocompleteModule,
@@ -43,51 +47,27 @@ import {
   MdToolbarModule,
   MdTooltipModule,
 } from '@angular/material';
-import {CdkTableModule} from '@angular/cdk';
+import { CdkTableModule } from '@angular/cdk';
 // service
-import {RatingPageComponent, RatingService,ApiService, DialogReviewEnter, AdminRatingPageComponent} from './pages/rating';
-import {CreateRatingComponent} from './pages/rating/admin-rating-page/create-rating/create-rating.component';
-import {RatingInfoComponent,RatingDeleteDialog} from './pages/rating/admin-rating-page/rating-info/rating-info.component';
+import { RatingPageComponent, RatingService, ApiService, DialogReviewEnter, AdminRatingPageComponent } from './pages/rating';
+import { CreateRatingComponent } from './pages/rating/admin-rating-page/create-rating/create-rating.component';
+import { RatingInfoComponent, RatingDeleteDialog } from './pages/rating/admin-rating-page/rating-info/rating-info.component';
 import { RatingChartComponent } from './pages/rating/admin-rating-page/rating-info/rating-chart/rating-chart.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import {
+  ApiService2,
+  AuthService,
+  UserService,
+  FooService,
+  ConfigService
+} from './service';
+import { LoginComponent } from './login';
 
-
-// @NgModule({
-//   exports: [
-//     CdkTableModule,
-//     MdAutocompleteModule,
-//     MdButtonModule,
-//     MdButtonToggleModule,
-//     MdCardModule,
-//     MdCheckboxModule,
-//     MdChipsModule,
-//     MdCoreModule,
-//     MdDatepickerModule,
-//     MdDialogModule,
-//     MdExpansionModule,
-//     MdGridListModule,
-//     MdIconModule,
-//     MdInputModule,
-//     MdListModule,
-//     MdMenuModule,
-//     MdNativeDateModule,
-//     MdPaginatorModule,
-//     MdProgressBarModule,
-//     MdProgressSpinnerModule,
-//     MdRadioModule,
-//     MdRippleModule,
-//     MdSelectModule,
-//     MdSidenavModule,
-//     MdSliderModule,
-//     MdSlideToggleModule,
-//     MdSnackBarModule,
-//     MdSortModule,
-//     MdTableModule,
-//     MdTabsModule,
-//     MdToolbarModule,
-//     MdTooltipModule,
-//   ]
-// })
+import { LoginGuard, AdminPage } from './guard';
+import { HomeComponent } from './home/home.component';
+export function initUserFactory(userService: UserService) {
+    return () => userService.initUser();
+}
 
 @NgModule({
   declarations: [
@@ -99,8 +79,27 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
     CreateRatingComponent,
     RatingInfoComponent,
     RatingChartComponent,
-    ErrorPageComponent
+    HeaderComponent,
+    ErrorPageComponent,
+    LoginComponent,
+    HomeComponent
   ],
+  entryComponents: [RatingDeleteDialog, DialogReviewEnter],
+  providers: [RatingService, ApiService, ApiService2, LoginGuard,
+    AuthService,
+    AdminPage,
+    UserService,
+    FooService,
+    ConfigService,
+  ,
+    {
+      'provide': APP_INITIALIZER,
+      'useFactory': initUserFactory,
+      'deps': [UserService],
+      'multi': true
+    }
+  ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -143,9 +142,6 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
     MdTabsModule,
     MdToolbarModule,
     MdTooltipModule,
-  ],
-  entryComponents: [RatingDeleteDialog,DialogReviewEnter],
-  providers: [RatingService,ApiService],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule {}
+export class AppModule { }
