71922b0aefacf344f9545219a1c5a85dacaab26e
sezarmbar <sezar_mbar@yahoo.com>
2017-08-05 12:32:53 +0200

    create ratin form fix

--- frontEnd/src/app/pages/rating/admin-rating-page/create-rating/create-rating.component.ts
+++ frontEnd/src/app/pages/rating/admin-rating-page/create-rating/create-rating.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit,Output,EventEmitter,Input} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {Rating} from "../../";
 
 @Component({
@@ -10,17 +11,30 @@ export class CreateRatingComponent implements OnInit {
   @Output() onDatePicked: EventEmitter<any> = new EventEmitter<any>();
   @Output() onCancle: EventEmitter<any> = new EventEmitter<any>();
   @Input() nameError: boolean;
-  constructor() {}
-  rating : Rating;
-  ngOnInit() {}
+  rating : Rating;  
+  form: FormGroup;  
+  submitted = false;
+  constructor(private formBuilder: FormBuilder,) {}
+  ngOnInit() {
 
-  readFormData(ratingName, description, active,waitingTime):void {
-    this.rating = new Rating(null,ratingName.value,description.value,"0","0","0","0","0",null,active.checked,waitingTime.value);
-    this.onDatePicked.emit(this.rating);
+    this.form = this.formBuilder.group({
+      ratingName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(16)])],
+      waitingTime: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(3)])],
+      description: ['', Validators.compose([ Validators.minLength(3), Validators.maxLength(64)])],
+      active: []
+    });
+
   }
+onSubmit(){
+  this.submitted = true;
+  const creatRD = this.form.value;
+  this.rating = new Rating(null,creatRD.ratingName,creatRD.description,"0","0","0","0","0",null,creatRD.active,creatRD.waitingTime);
+  this.onDatePicked.emit(this.rating);
+}
 
   onCansle(){
     this.onCancle.emit();
+    console.log(this.form.value)
   }
 
 }
